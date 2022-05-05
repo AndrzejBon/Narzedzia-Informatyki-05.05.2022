631ac8d6c30a232881880e614b061b6f0ebb0db8
Andrzej Bończyszyn <baczek1133@gmail.com>
2022-05-05 17:43:17 +0200

    Zad_1 z zajec 1

---
+++ Zad_1.java
@@ -0,0 +1,9 @@
+public class Zad_1 {
+    public static void main(String[] args){
+    int cal = 2;
+    float zmienno = 2.2f;
+    String sd = "123";
+    float wynik = (float)cal + (float)zmienno + Float.valueOf(sd);
+    System.out.print(wynik);
+  }
+}
